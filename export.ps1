5382acdd2a734414defd426ca50cba8f45130c8a
beefarino <jim.christopher@gmail.com>
2019-10-06 16:26:54 -0400

    added export script; outcome data

---
+++ export.ps1
@@ -0,0 +1,16 @@
+param( $theme );
+
+$blob = $theme;
+if( -not $theme ) {
+  $theme = ls ./node_modules/jsonresume-theme-* | select -expand name ;
+  $theme = $theme -replace 'jsonresume-theme-', '';
+  write-warning ("no theme specified, exporting all {0} themes installed" -f $theme.count )
+  $blob = '*';
+}
+
+@($theme) | foreach {
+  $name = $_;
+  node .\node_modules\resume-cli export --theme=${name}  jim-christopher-${name}.html
+}
+
+ls jim-christopher-${blob}.html | ii;
