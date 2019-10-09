4bfcae9be04b794c5e58086950d9219a1a1e3f8a
beefarino <jim.christopher@gmail.com>
2019-10-09 00:15:29 -0400

    fixed dates on work and education; played with style a bit

--- jsonresume-theme-jimbo-simple/index.js
+++ jsonresume-theme-jimbo-simple/index.js
@@ -4,6 +4,8 @@ var Handlebars = require("handlebars");
 function render(resume) {
 	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
 	var template = fs.readFileSync(__dirname + "/resume.template", "utf-8");
+	Handlebars.registerHelper( "yearOf", function(d){ return new Date(d).getFullYear(); })
+
 	return Handlebars.compile(template)({
 		css: css,
 		resume: resume
